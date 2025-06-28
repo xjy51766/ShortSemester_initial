10bbb021048ff96391ee837007b9bd268bbca529
xjy51766 <213721483@qq.com>
2025-06-28 13:39:55 +0800

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,12 @@
+create table gesture_violation_db.violation_record
+(
+    id             int auto_increment
+        primary key,
+    createTime     datetime              not null,
+    gestureClass   varchar(50)           not null,
+    judgeType      enum ('自动', '人工') not null,
+    screenshotPath varchar(100)          null
+)
+    collate = utf8mb4_unicode_ci;
+
+
